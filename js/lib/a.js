dc6e78cf9fb6e3f807787a80c88d9896f04be507
willhu85 <willhu85@gmail.com>
2014-12-01 20:30:59 +0800

    h5slides

---
+++ js/lib/a.js
@@ -0,0 +1,11 @@
+/**
+ * Created by willhu on 2014/11/25.
+ */
+define(function(){
+	var a = "fun1";
+	function fun1(){
+		//alert("fun1 is work");
+	}
+	fun1();
+	return a;
+})
---
+++ js/slides.js
@@ -0,0 +1,23 @@
+/**
+ * Created by willhu on 2014/12/01
+ */
+//require.config({
+//	baseUrl: "../js/",
+//	paths : {
+//		"jquery" : ["http://libs.baidu.com/jquery/2.0.3/jquery", "/lib/jquery-1.11.1"],
+//		"a": ["lib/a"]
+//	}
+//});
+//require(["jquery", "a"],function($, a) {
+//	$(function() {
+//		alert(a)
+//	});
+//});
+
+;(function($)) {
+	$.extend({
+		wwSlide: {
+			// 配置项
+		}
+	})
+})(jQuery);
